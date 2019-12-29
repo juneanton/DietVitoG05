0259717bd919b5d72292faa7eb08b688249462a4
June <June@LAPTOP-1239LB0V>
2019-12-29 17:01:15 +0100

    .

--- src/java/packDietVito/Servlet_Login.java
+++ src/java/packDietVito/Servlet_Login.java
@@ -40,6 +40,7 @@ public class Servlet_Login extends HttpServlet {
                 request.getSession().setAttribute("contraseña", request.getParameter("contraseña"));
                 //cambia la pagina
                 //request.getRequestDispatcher("Dietista.jsp").forward(request,response); 
+                //System.out.println("muestra dietista");
                 response.sendRedirect("Dietista.jsp");
             }
             else if (buscarEmail(email) && buscarContraseña(contraseña)) {
--- web/Dietista.jsp
+++ web/Dietista.jsp
@@ -5,6 +5,7 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="packDietVito.Servlet_Login" %>
 <!DOCTYPE html>
 <html>
     <head>
