edb3a3040180592131652436dd100df3ecef3b96
miren <miren@LAPTOP-3T3T0SFM>
2019-12-30 16:43:00 +0100

    registrar usu

--- src/java/packDietVito/Servlet_Actividades.java
+++ src/java/packDietVito/Servlet_Actividades.java
@@ -79,7 +79,7 @@ public class Servlet_Actividades extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doGet(request, response);
     }
 
     /**
